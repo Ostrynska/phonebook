3a1432e6f6007197424b0447a971a8b6efaa2728
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-07-12 14:15:37 +0300

    Update operations.jsx

--- src/redux/auth/operations.jsx
+++ src/redux/auth/operations.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://phonebook-project.onrender.com/';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+// axios.defaults.baseURL = 'https://nodejs-rest-api-whud.onrender.com/';
 
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
