7c0dac861f96ef459fac73adb0f3842efb9c9993
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-07-22 01:16:21 +0300

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { useAuth } from '../../hooks/index';
 
 import { Navigation } from '../Navigation/Navigation';
@@ -6,10 +7,21 @@ import { AuthNav } from '../AuthNav/AuthNav';
 
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import { AppBarStyled, Link, AppToolbar } from './AppBar.styled';
+import {
+  AppBarStyled,
+  Link,
+  AppToolbar,
+  MenuIcon,
+  DrawerStyled,
+} from './AppBar.styled';
 
 export const AppBarNavigation = () => {
   const { isLoggedIn } = useAuth();
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = isOpen => () => {
+    setOpen(isOpen);
+  };
 
   return (
     <AppBarStyled>
@@ -29,6 +41,22 @@ export const AppBarNavigation = () => {
           </Link>
           <Navigation />
           {isLoggedIn ? <UserMenu /> : <AuthNav />}
+          <MenuIcon
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </MenuIcon>
+          <DrawerStyled
+            anchor="right"
+            open={open}
+            onClose={toggleDrawer(false)}
+          >
+            <Navigation />
+            {isLoggedIn ? <UserMenu /> : <AuthNav />}
+          </DrawerStyled>
         </AppToolbar>
       </Container>
     </AppBarStyled>
